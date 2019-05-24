3991a85805efc85a801ad11222e5b8152a416d50
Ragupathy S <ragupathy.s@cognizant.com>
2019-05-24 17:25:43 +0530

    Upload New File

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // MongoURI: 'mongodb+srv://ragupats:<ragu1234>@expresscluster-093zj.mongodb.net/test?retryWrites=true'
+    MongoURI: "mongodb://localhost:27017/db",
+    JWT_SECRET: 'addjsonwebtokensecretherelikeQuiscustodietipsoscustodes'
+}
